8b52467398af9fae76eda1dc8da1c5a83b62f02f
Fabian Tschirschnitz <fatschi@googlemail.com>
2014-05-28 11:50:55 +0200

    first commit of freebase crawl

---
+++ src/main/java/com/freebase/samples/FreebasePartyTenures.java
@@ -0,0 +1,69 @@
+package com.freebase.samples;
+
+import java.io.PrintWriter;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.cedarsoftware.util.io.JsonWriter;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.jayway.jsonpath.JsonPath;
+
+public class FreebasePartyTenures {
+  public static void main(String[] args) {    
+    try {
+        HttpTransport httpTransport = new NetHttpTransport();
+        HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+        JSONParser parser = new JSONParser();
+        //MQL query
+        /*
+        [{
+			  "id": null,
+			  "type": "/government/political_party_tenure",
+			  "politician": {
+			  },
+			  "party": {
+			  },
+			  "from": null,
+			  "to": null
+		}]
+         */
+        String queryTemplate = "[{  \"id\": null,  \"type\": \"/government/political_party_tenure\",  \"politician\": {  },  \"party\": {  },  \"from\": null,  \"to\": null, \"limit\":100\n}]";
+        String currentCursor = "";
+        GenericUrl url = new GenericUrl("https://www.googleapis.com/freebase/v1/mqlread");
+        JSONArray finalResult = new JSONArray();
+        while(true){
+        	url.put("query", queryTemplate);
+        	url.put("cursor", currentCursor);
+            url.put("key", "AIzaSyB2rARUGp8xWNQ5p_U4Yh1Xxs_e-dR9Cws");
+            System.out.println(url.toString());
+            HttpRequest request = requestFactory.buildGetRequest(url);
+            HttpResponse httpResponse = request.execute();
+            JSONObject response = (JSONObject)parser.parse(httpResponse.parseAsString());
+            
+            JSONArray results = (JSONArray)response.get("result");
+            System.out.println(currentCursor);
+            for (Object result : results) {
+              System.out.println(JsonPath.read(result,"$.id").toString());
+              finalResult.add(result);
+            }
+            if(response.get("cursor").equals(Boolean.FALSE)){
+            	break;
+            }else{
+            	currentCursor = "="+(String) response.get("cursor");
+            }
+        }
+        PrintWriter out = new PrintWriter("freebasePartyTenures.json");
+        out.print(JsonWriter.formatJson(finalResult.toJSONString()));
+        out.close();
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+  }
+}
